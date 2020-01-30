1f092d4065c0926c6326fef63740b2828a71b795
Manngold <jsw4820@gmail.com>
2020-01-30 20:02:37 +0900

    200130 React study record

--- src/App.js
+++ src/App.js
@@ -4,11 +4,31 @@ import Navigation from "./Components/Navigation";
 import Content from "./Components/Content";
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            mode: "welcome",
+            subject: { title: "WEB", sub: "World Wide Web" },
+            welcome: { title: "Welcome", desc: "Hello React!!" },
+            contents: [
+                { id: 1, title: "HTML", desc: "HTML is for information" },
+                { id: 2, title: "CSS", desc: "CSS is for design" },
+                {
+                    id: 3,
+                    title: "JavaScript",
+                    desc: "JavaScript is for interactive"
+                }
+            ]
+        };
+    }
     render() {
         return (
             <div className="App">
-                <Subject title="WEB" sub="world wide web!"></Subject>
-                <Navigation list={["HTML", "CSS", "JavaScript"]}></Navigation>
+                <Subject
+                    title={this.state.subject.title}
+                    sub={this.state.subject.sub}
+                ></Subject>
+                <Navigation data={this.state.contents}></Navigation>
                 <Content
                     title="HTML"
                     desc="HTML is HyperText Mark-up Language"
